e502941de63f8896e476a5afb07a6f390bd0209f
Marvin <marvin.k.scharding@gmail.com>
2019-11-20 18:43:54 +0100

    Started building level on the new scene

---
+++ Assets/scripts/objectsScripts/rock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour
+{
+    [SerializeField] int damage = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerHealth>() != null)
+        {
+            PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(damage);
+        }
+
+    }
+}
