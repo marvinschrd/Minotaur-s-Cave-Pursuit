43ba9e861827aa8c957fe98d8404909b50ebeda1
Marvin <marvin.k.scharding@gmail.com>
2019-11-25 17:57:41 +0100

    added next trap : crushing blocks first version

--- Assets/scripts/objectsScripts/CrushingBlock.cs
+++ Assets/scripts/objectsScripts/CrushingBlock.cs
@@ -5,35 +5,106 @@ using UnityEngine;
 public class CrushingBlock : MonoBehaviour
 {
     [SerializeField] private int damage = 200;
-    [SerializeField] float timeToFall = 0.05f;
-    private float timerToFall = 0f;
+    [SerializeField] float timerToFall = 0.05f;
+    private float timeToFall = 0f;
+    [SerializeField] float TimerToUp;
+    private float timeToUp;
+
     private Rigidbody2D body;
     private bool isTriggered = false;
+    private Vector3 upPosition;
+    [SerializeField] float speed;
     private void Start()
     {
         body = transform.GetComponent<Rigidbody2D>();
+        upPosition = transform.position;
     }
+
+    enum State
+    {
+        IDLE,
+        FALLING,
+        WAITING,
+        MOVING_UP
+    }
+
+    State state = State.IDLE;
 
     private void Update()
     {
-        TimeBeforeFalling();
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        isTriggered = true;
-    }
-    void TimeBeforeFalling()
-    {
-        if (isTriggered == true)
+        //TimeBeforeFalling();
+        switch (state)
         {
-            timeToFall -= Time.deltaTime;
-            Debug.Log(timeToFall);
-            if (timeToFall <= 0)
-            {
-                body.bodyType = RigidbodyType2D.Dynamic;
-            }
+            case State.IDLE:
+                Debug.Log("IDLE");
+                timeToFall = timerToFall;
+                timeToUp = TimerToUp;
+                isTriggered = false;
+                body.bodyType = RigidbodyType2D.Static;
+                break;
+            case State.FALLING:
+                {
+                    Debug.Log("FALLING");
+                    timeToFall -= Time.deltaTime;
+                    Debug.Log(timeToFall);
+                    if (timeToFall <= 0)
+                    {
+                        body.bodyType = RigidbodyType2D.Dynamic;
+                    }
+                    if (isTriggered)
+                    {
+                        state = State.WAITING;
+                    }
+                }
+                break;
+            case State.WAITING:
+                {
+                    Debug.Log("isWaiting");
+
+                    timeToUp -= Time.deltaTime;
+                    Debug.Log(timeToUp);
+                    if (timeToUp <= 0)
+                    {
+                        state = State.MOVING_UP;
+                    }
+
+                }
+                break;
+            case State.MOVING_UP:
+                {
+                    Debug.Log("moving Up");
+                    body.velocity = (upPosition - transform.position).normalized * speed;
+                    if (Vector3.Distance(transform.position, upPosition) < 0.05f)
+                    {
+                        state = State.IDLE;
+                    }
+                    break;
+                }
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        state = State.FALLING;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    //isTriggered = true;
+
+    //}
+    //void TimeBeforeFalling()
+    //{
+    //    if (isTriggered == true)
+    //    {
+    //        timeToFall -= Time.deltaTime;
+    //        Debug.Log(timeToFall);
+    //        if (timeToFall <= 0)
+    //        {
+    //            body.bodyType = RigidbodyType2D.Dynamic;
+    //        }
+    //    }
+    //}
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerHealth>() != null)
@@ -41,6 +112,6 @@ public class CrushingBlock : MonoBehaviour
             PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
             playerHealth.TakeDamage(damage);
         }
-        isTriggered = false;
+        isTriggered = true;
     }
 }
