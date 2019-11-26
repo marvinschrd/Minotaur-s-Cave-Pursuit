c07529d8d156ea19dd8e0193ddaa69a7f4b6ebc6
Marvin <marvin.k.scharding@gmail.com>
2019-11-26 18:36:14 +0100

    respawn mechanism for cart +  cart behavior into state machine

--- Assets/scripts/objectsScripts/cart.cs
+++ Assets/scripts/objectsScripts/cart.cs
@@ -9,41 +9,91 @@ public class cart : MonoBehaviour
    private Vector2 direction;
    private bool isTriggered = false;
    [SerializeField] float speed;
-   
+    private Vector2 startingPosition;
+    [SerializeField] float timerToSpawnBack;
+    private float timeToSpawnBack;
+
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
+       
     }
 
-    private void FixedUpdate()
+   enum State
     {
-        
+        IDLE,
+        MOVING,
+        WAITING,
+        SPAWNING_BACK
     }
+    State state = State.IDLE;
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (isTriggered)
+        //Debug.Log(state);
+        //if (isTriggered)
+        //{
+
+        //   if(body.velocity.magnitude <1)
+        //    {
+        //       // Debug.Log(body.velocity.y);
+        //        direction = new Vector2(speed, body.velocity.y);
+        //        body.velocity = direction;
+        //    }
+
+        //}
+
+        switch (state)
         {
-            
-           if(body.velocity.magnitude <1)
-            {
-               // Debug.Log(body.velocity.y);
-                direction = new Vector2(speed, body.velocity.y);
-                body.velocity = direction;
-            }
+            case State.IDLE:
+                timeToSpawnBack = timerToSpawnBack;
+                break;
+            case State.MOVING:
+                if (body.velocity.magnitude < 1)
+                {
+                    // Debug.Log(body.velocity.y);
+                    direction = new Vector2(speed, body.velocity.y);
+                    body.velocity = direction;
+                }
+                break;
+            case State.WAITING:
+                timeToSpawnBack -= Time.deltaTime;
+                if (timeToSpawnBack <= 0)
+                {
+                    state = State.SPAWNING_BACK;
+                }
+                break;
+            case State.SPAWNING_BACK:
+                transform.position = startingPosition;
+                state = State.IDLE;
+                break;
 
         }
-        
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         isTriggered = true;
-        
+        state = State.MOVING;
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Debug.Log("exit");
+        isTriggered = false;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Debug.Log(collision.gameObject.tag);
+        if(collision.gameObject.tag == "cartStopper" && !isTriggered)
+        {
+            //Debug.Log("WAITING");
+            state = State.WAITING;
+        }
+    }
 }
